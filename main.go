bb35089166ba6dcc8c86de91725ee76a50360a1f
Homero Araujo | S2IT <homero.araujo@s2it.corp>
2020-03-13 12:11:21 -0300

    CORS problem solved

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 // função principal
@@ -15,7 +16,17 @@ func main() {
 
 	router.HandleFunc("/join", ParticipantJoined).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Content-Type", "Access-Control-Allow-Origin", "Accept"},
+		ExposedHeaders:   []string{"Content-Type", "Access-Control-Allow-Origin", "Accept"},
+		AllowedMethods:   []string{"POST"},
+	})
+
+	handler := c.Handler(router)
+
+	log.Fatal(http.ListenAndServe(":8000", handler))
 }
 
 type Post struct {
